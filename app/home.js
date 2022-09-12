4cb981ff76796575f55b540e49f230aee7ec996f
AdamHajinJoo <godisperpect@gmail.com>
2022-09-13 01:26:25 +0900

    home.js title, content implemented. but i dunno why boldBtn doesn't work

--- app/home.js
+++ app/home.js
@@ -4,20 +4,26 @@ document.querySelector("#year").innerHTML = today.getFullYear();
 document.querySelector("#month").innerHTML = clockNotationConverter(today.getMonth() + 1);
 document.querySelector("#d_date").innerHTML = clockNotationConverter(today.getDate());
 
-const spaceToWrite = document.querySelector('#spaceToWrite');
-const colorBtn = document.querySelector('colorBtn');
-const fontBtn = document.querySelector('fontBtn');
-const boldBtn = document.querySelector('boldBtn');
-const italicBtn = document.querySelector('italicBtn');
+const spaceToWrite = document.querySelector("#spaceToWrite");
+const colorBtn = document.querySelector("#colorBtn");
+const fontBtn = document.querySelector("#fontBtn");
+const boldBtn = document.querySelector("#boldBtn");
+const italicBtn = document.querySelector("#italicBtn");
 
-boldBtn.addEventListener('click', () => { document.execCommand('bold') }
-
-)
+boldBtn.addEventListener("click", function() {
+    document.execCommand("bold")
+})
+italicBtn.addEventListener("click", () => {
+    document.execCommand("italic")
+})
+colorBtn.addEventListener("input", () => {
+    document.execCommand("forecolor", false, colorBtn.value)
+})
 
 function clockNotationConverter(num) {
     let stringNum = String(num)
     if (stringNum.length <= 1) {
-        stringNum = '0'+ stringNum
+        stringNum = "0"+ stringNum
     }
     return stringNum
 }
